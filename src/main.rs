e390348f1c1e618ab5082ef905c128f14958acae
William Setiawan <willc0d3r@gmail.com>
2026-04-19 13:27:28 +1000

    Setup basic application state struct.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum VerificationStatus {
+    Idle,
+    Hashing,
+    Match,
+    NoMatch,
+    Error(String),
+}
+
+impl Default for VerificationStatus {
+    fn default() -> Self {
+        Self::Idle
+    }
+}
+
+#[derive(Default)]
+pub struct AppState {
+    pub file_path: Option<String>,
+    pub expected_hash: String,
+    pub computed_hash: Option<String>,
+    pub progress: f32,
+    pub status: VerificationStatus,
+}
+
 fn main() {
     println!("Hello, world!");
 }
