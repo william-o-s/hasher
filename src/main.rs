4581749febf59d3bdebdb4801501c6b6cff544b2
William Setiawan <willc0d3r@gmail.com>
2026-04-22 22:30:30 +1000

    Added color palette to app.

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,21 @@ fn main() -> eframe::Result<()> {
     eframe::run_native(
         "File Hasher",
         native_options,
-        Box::new(|_cc| Ok(Box::new(AppState::default()))),
+        Box::new(|cc| {
+            cc.egui_ctx.set_visuals(get_visuals());
+            Ok(Box::new(AppState::default()))
+        }),
     )
+}
+
+fn get_visuals() -> egui::Visuals {
+    let mut visuals = eframe::egui::Visuals::dark();
+    visuals.window_fill = eframe::egui::Color32::from_rgb(26, 27, 38);
+    visuals.widgets.noninteractive.bg_fill = eframe::egui::Color32::from_rgb(36, 37, 54);
+    visuals.widgets.inactive.bg_fill = eframe::egui::Color32::from_rgb(46, 48, 71);
+    visuals.widgets.hovered.bg_fill = eframe::egui::Color32::from_rgb(66, 68, 101);
+    visuals.widgets.active.bg_fill = eframe::egui::Color32::from_rgb(86, 88, 131);
+    visuals.selection.bg_fill = eframe::egui::Color32::from_rgb(65, 105, 225);
+
+    visuals
 }
